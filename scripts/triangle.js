aeb49b220fa32ed91b367da17c23a0bd6295e866
sumaiyaemu <sumaiyasarkeremu143@gmail.com>
2024-02-12 23:09:53 +0600

    create a index html file with dom

---
+++ scripts/triangle.js
@@ -0,0 +1,17 @@
+function calculateTriangleArea(){
+    const triangleBaseInput = document .getElementById('traiangle-base')
+    const triangleBaseText = triangleBaseInput. value;
+    const base= parseFloat(triangleBaseText)
+    //console.log(base)
+
+    const triangleHeightInput = document .getElementById('traiangle-height')
+    const triangleHeightText = triangleHeightInput. value;
+    const height = parseFloat(triangleHeightText)
+    //console.log(height)
+    const area = 0.5 * base * height;
+    //console.log('area of he triangle is:',area)
+
+    const triangleAreaSpan = document .getElementById('triangle-area')
+    triangleAreaSpan .innerText = area;
+    //console .log(triangleAreaSpan)
+}
